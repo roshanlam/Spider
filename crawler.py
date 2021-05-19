e2113273c452c31986d13204aa1355e60fdde6c1
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2021-05-19 11:46:39 -0400

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,40 @@
+import requests, pathlib
+from bs4 import BeautifulSoup
+import json
+import requests
+
+def crawl(url, depth, filename):
+    try:
+        response = requests.get(url)
+    except:
+        print('Failed to perform HTTP GET request on "%s"\n' % url)
+        return
+    website = BeautifulSoup(response.text, 'lxml')
+    try:
+        title = website.find('title').text
+        paragraph = ''
+        h1 = ''
+        a = ''
+        div = ''
+        for tag in website.findAll():
+            if tag.name == 'p':
+                paragraph += tag.text.strip().replace('\n', '')
+            if tag.name == 'h1':
+                h1 += tag.text.strip().replace('\n', '')
+            if tag.name == 'a':
+                a += tag.text.strip().replace('\n', '')
+            if tag.name == 'div':
+                div += tag.text.strip().replace('\n', '')
+    except:
+        return
+    result = {
+        'url': url,
+        'title': title,
+        'paragraph': paragraph,
+        'header1' : h1,
+        'a': a,
+        'div' : div
+    }
+    return result.values()
+
+print(crawl('https://roshanlamichhane.tech', 5, 'roshan'))
